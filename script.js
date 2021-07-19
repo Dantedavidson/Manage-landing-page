ed5d51192062208eabd3dcfe4f60e7ed58df5410
Dantedavidson <50653094+Dantedavidson@users.noreply.github.com>
2021-07-19 14:34:26 +1000

    Mobiler testimonial animations

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const toggleBg = document.querySelector(".header__toggle-bg");
 const toggleNav = document.querySelector(".header__toggle-box");
 
 hamburgerBtn.addEventListener("click", () => {
-  console.log("i went off");
   if (hamburgerBtn.classList.contains("header__hamburger--active")) {
     hamburgerBtn.classList.remove("header__hamburger--active");
 
@@ -27,3 +26,106 @@ hamburgerBtn.addEventListener("click", () => {
     toggleNav.classList.add("fade-in");
   }
 });
+
+let cards = Array.from(document.querySelectorAll(".testimonials__cards .card"));
+let mobileTogle = Array.from(
+  document.querySelectorAll(".testimonials__display-bar span")
+);
+
+class Testimonials {
+  constructor(cards, selector) {
+    this.active = 0;
+    this.screenSize = screen.width;
+    this.cards = cards;
+    this.selector = selector;
+  }
+
+  //card animations
+  exitRight(card) {
+    card.addEventListener("animationend", function animationEnd() {
+      card.classList.remove("card--active");
+      card.classList.remove("exit-right");
+      card.removeEventListener("animationend", animationEnd);
+    });
+    card.classList.add("exit-right");
+  }
+
+  exitLeft(card) {
+    card.addEventListener("animationend", function animationEnd() {
+      card.classList.remove("card--active");
+      card.classList.remove("exit-left");
+      card.removeEventListener("animationend", animationEnd);
+    });
+    card.classList.add("exit-left");
+  }
+
+  enterRight(card) {
+    card.addEventListener("animationend", function animationEnd() {
+      card.classList.add("card--active");
+      card.classList.remove("enter-right");
+      card.removeEventListener("animationend", animationEnd);
+    });
+    card.classList.add("enter-right");
+  }
+
+  enterLeft(card) {
+    card.addEventListener("animationend", function animationEnd() {
+      card.classList.add("card--active");
+      card.classList.remove("enter-left");
+      card.removeEventListener("animationend", animationEnd);
+    });
+    card.classList.add("enter-left");
+  }
+
+  //card change
+  handleToggleClick(id) {
+    if (id > this.active) {
+      // //set target to enter right. set active to exit left
+      this.enterRight(this.cards[id]);
+      this.exitLeft(this.cards[this.active]);
+    } else {
+      //set target to enter left. set active to exit right
+      this.enterLeft(this.cards[id]);
+      this.exitRight(this.cards[this.active]);
+    }
+    this.active = parseInt(id);
+    this.selector.forEach((span, index) => this.setActiveBubble(span, index));
+  }
+
+  handleInterval() {
+    let current = this.active;
+    this.active = this.active >= 3 ? 0 : this.active + 1;
+
+    this.enterRight(this.cards[this.active]);
+    this.exitLeft(this.cards[current]);
+
+    this.selector.forEach((span, index) => this.setActiveBubble(span, index));
+  }
+
+  setActiveBubble(span, index) {
+    if (index === this.active) {
+      span.classList.add("dot--active");
+    } else {
+      span.classList.remove("dot--active");
+    }
+  }
+
+  setActiveCard() {
+    this.cards[this.active].classList.add("card--active");
+    this.selector[this.active].classList.add("dot--active");
+  }
+}
+
+let testimonialController = new Testimonials(cards, mobileTogle);
+
+testimonialController.setActiveCard();
+
+mobileTogle.forEach((toggler) => {
+  toggler.addEventListener("click", () => {
+    testimonialController.handleToggleClick(toggler.id);
+  });
+});
+
+window.setInterval(() => {
+  testimonialController.handleInterval();
+}, 15000);
